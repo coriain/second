a969681d033f4c8bbe671c75d7c29fea9410e63e
Colm O'Riain <coriain@zynga.com>
2012-12-10 13:36:05 -0800

    Initial commit of Second sample app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Second::Application.config.secret_token = '3a7f21f44f8ae2eb9ffbe68a9ffb555cfc81a9460acdf74dda8cc5896df5c94cf8b73f390a6dcc066d2593426f5715342381b579967f38d16f45341a65bdd674'
